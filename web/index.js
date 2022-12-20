bf9fb3f97ff1bae1f5249715f7eafb3132e9e184
chrisarmyknife <chrisdarmody@gmail.com>
2022-12-20 02:43:22 -0500

    Fix hidden audio div bug

--- web/index.js
+++ web/index.js
@@ -146,8 +146,8 @@ function get_audio_support() {
     call_endpoint("/is_audio_supported", function (value) {
         console.log("Is audio supported? " + value);
         audio_supported = value;
-        document.getElementById("mic_button").hidden = ~value;
-        document.getElementById("sounds_div").hidden = ~value;
+        document.getElementById("mic_button").hidden = !value;
+        document.getElementById("sounds_div").hidden = !value;
         if (value) {
             populate_sound_dropdown();
         }
