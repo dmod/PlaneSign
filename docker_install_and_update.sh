4058425dfd1e09140b4425676656f76229fcca82
dmod <adarmody@gmail.com>
2023-12-24 01:14:13 -0500

    Adding -y switch

--- docker_install_and_update.sh
+++ docker_install_and_update.sh
@@ -31,7 +31,7 @@ crontab -r
 
 # Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
+sudo apt-get -y install ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --batch --yes --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
@@ -44,6 +44,10 @@ echo \
 sudo apt-get update
 
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo groupadd --force docker
+sudo usermod -aG docker $USER
+# newgrp docker
 
 sudo systemctl enable docker.service
 sudo systemctl enable containerd.service
