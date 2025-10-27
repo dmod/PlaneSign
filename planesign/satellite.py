49487bc6b0ff05256914ec92de03456529d89530
chrisarmyknife <chrisdarmody@gmail.com>
2025-10-26 22:19:20 -0400

    More sat special cases

--- planesign/satellite.py
+++ planesign/satellite.py
@@ -138,15 +138,15 @@ def get_flag(selected,satellite_data):
     USA_names = ["AEROCUBE ", "COMSTAR", "DIRECTV", "DMSP ", "DOVE ", "ECHOSTAR", "ESSA ",
                  "EXPLORER", "FLOCK", "GALAXY", "GLOBAL-", "GLOBALSTAR", "GOES ", "HAWK-",
                  "INTELSAT", "IRIDIUM", "KUIPER", "LANDSAT", "LEASAT ", "LEMUR 2", "LES ",
-                 "NAVSTAR", "NOAA", "OPS ", "ORBCOMM", "SATCOM", "SECOR ", "SPACEBEE-",
-                 "STARLINK", "TIROS ", "USA", "WESTFORD NEEDLES"]
+                 "NAVSTAR", "NOAA", "OPS ", "ORBCOMM", "PRAETORIAN", "SATCOM", "SECOR ",
+                 "SPACEBEE-", "STARLINK", "TIROS ", "USA", "WESTFORD NEEDLES"]
 
     RUS_names = ["COSMOS", "EKRAN", "EXPRESS", "GONETS", "GORIZONT", "KANOPUS",
                  "MOLNIYA", "METEOR", "NADEZHDA", "OKEAN ", "RADIO ", "RADUGA",
                  "SITRO-", "YUZGU"]
 
     CHN_names = ["BEIDOU", "CENTISPACE", "CHINASAT", "FENGYUN", "GAOFEN", "GEESAT",
-                 "HULIANWANG", "JILIN", "QIANFAN", "SINOSAT", "SHIYAN", "TIANHUI",
+                 "HJS-", "HULIANWANG", "JILIN", "QIANFAN", "SINOSAT", "SHIYAN", "TIANHUI",
                  "TIANLIAN", "TIANMU", "TIANQI", "TIANZHOU", "YAOGAN", "YUNHAI", ]
 
     GBR_names = ["INMARSAT", "O3B", "ONEWEB", "SKYNET "]
@@ -244,6 +244,9 @@ def get_flag(selected,satellite_data):
 
         elif sat_name.find("TEVEL") == 0:
             code = "ISR"
+
+        elif sat_name.find("FLORIPASAT") == 0:
+            code = "BRA"
 
         #Still can't find country: go scrape country from website
         else:
