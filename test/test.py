dae8fd23c43093863efbdefb95fed273ff735b85
Andrew <adarmody@gmail.com>
2026-02-12 05:19:04 +0000

    Adding new launch target, updating airports

--- test/test.py
+++ test/test.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-from rgbmatrix import graphics, RGBMatrix, RGBMatrixOptions
-from PIL import Image
+from rgbmatrix import RGBMatrix, RGBMatrixOptions
 import time
 
 options = RGBMatrixOptions()
@@ -16,19 +15,12 @@ options.hardware_mapping = "adafruit-hat"
 myMatrix = RGBMatrix(options = options)
 myCanvas = myMatrix.CreateFrameCanvas()
 
-font57 = graphics.Font()
-font57.LoadFont('/home/pi/rpi-rgb-led-matrix/fonts/4x6.bdf')
-
-#graphics.DrawText(myCanvas, font57, 10, 18, graphics.Color(60, 60, 160), "Hellllooooooo")
-#
-
-
-image = Image.open(f"/home/pi/ind.png").convert('RGB')
-image = image.resize((25, 20), Image.BICUBIC).convert('RGB')
-myCanvas.SetImage(image, 20, 1)
+# Set every pixel to white
+width = options.cols * options.chain_length
+height = 32
+for x in range(width):
+    for y in range(height):
+        myCanvas.SetPixel(x, y, 255, 255, 255)
 myCanvas = myMatrix.SwapOnVSync(myCanvas)
-#image = image.resize((20, 20))
-#myCanvas.SetImage(image, 10, 10)
-#image.thumbnail((64, 32), Image.ANTIALIAS)
 
-time.sleep(15)
+time.sleep(90)
